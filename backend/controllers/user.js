852d0637aa3f3ed57deb641b30d7d193ef2563f0
Roshan Singh <Surender@trimir.in>
2023-05-18 14:51:58 +0530

    change done

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -63,7 +63,12 @@ const logoutUser = asyncHandler(async (req, res) => {
 //@access private
 
 const getUserProfile = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "get user" });
+  const user = {
+    id: req.user._id,
+    name: req.user.name,
+    email: req.user.email,
+  };
+  res.status(200).json(user);
 });
 
 //@desc  update user profile
@@ -71,6 +76,26 @@ const getUserProfile = asyncHandler(async (req, res) => {
 //@access private
 
 const updateUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+    if (req.body.password) {
+      user.password = req.body.password;
+    }
+
+    const updatedUser = await user.save();
+
+    res.json({
+      id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
+
   res.status(200).json({ message: "update user" });
 });
 
